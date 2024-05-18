aa0e08b23ec32dc7ee5a63e162bc1256eaf8c1c9
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-05-17 21:18:48 -0600

    Create index.html, Add functions getComputerChoice(), getHumanChoice()

---
+++ script.js
@@ -0,0 +1,28 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3 + 1);
+  if (choice === 1) {
+    return "rock";
+  } else if (choice === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  let choice = prompt("Enter Rock, Paper or Scissors");
+  if (
+    choice.toLowerCase() == "rock" ||
+    choice.toLowerCase() == "paper" ||
+    choice.toLowerCase() == "scissors"
+  ) {
+    return choice.toLowerCase();
+  } else {
+    return "Invalid choice, pick Rock, Paper or Scissors!";
+  }
+}
+
+
